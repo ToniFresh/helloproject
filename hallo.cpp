34ef34f5dd4a3cda7820d0d1093f1eadae03aa57
Anton Matic <anton.matic@bulme.at>
2021-10-01 13:03:07 +0200

    interactive input

--- hallo.cpp
+++ hallo.cpp
@@ -7,7 +7,13 @@ using namespace std;
 
 int main()
 {
+	string name;
 	cout << "Hallo World!" << endl;
+	cout << "Wie heißt du?";
+	cin >> name;
+	cout << "Hallo " << name << endl;
+	
+	
 	
 	return 0;
 }
